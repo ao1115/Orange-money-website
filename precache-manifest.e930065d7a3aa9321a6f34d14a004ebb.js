e1113168134afc12ef71b128c35722687f25e337
aod <1677120510@qq.com>
2021-08-07 23:32:56 +0800

    deploy

---
+++ precache-manifest.e930065d7a3aa9321a6f34d14a004ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5d202069c853ab1bde",
+    "url": "/Orange-money-website/css/app.ccdaa721.css"
+  },
+  {
+    "revision": "7fdfb2bd2afd4d5ab3aaad3b24330a10",
+    "url": "/Orange-money-website/index.html"
+  },
+  {
+    "revision": "6c5d202069c853ab1bde",
+    "url": "/Orange-money-website/js/app.13168f42.js"
+  },
+  {
+    "revision": "0738be14dac42b786292",
+    "url": "/Orange-money-website/js/chunk-vendors.992a7a5b.js"
+  },
+  {
+    "revision": "5a3bc337ed9627c080692ef3c952b5ba",
+    "url": "/Orange-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Orange-money-website/robots.txt"
+  }
+]);
